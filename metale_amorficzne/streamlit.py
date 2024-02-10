f1d873c6e5120037f3ea1b16f6ed08bf828bd25e
Katarzyna Foszcz <kasia@foszcz.co>
2024-02-10 19:47:04 +0100

    Extend description
    - Introduction
    - Methods and Results (WIP)
    - Discussion (WIP)
    - References

--- metale_amorficzne/streamlit.py
+++ metale_amorficzne/streamlit.py
@@ -4,4 +4,6 @@ from pathlib import Path
 cur_dir = Path(__file__).parent
 
 st.title("Amorphous Metals Analyzer")
-st.markdown((cur_dir / "description/01_general.md").read_text("utf8"))
+st.markdown(
+    (cur_dir / "description/01_general.md").read_text("utf8"), unsafe_allow_html=True
+)
